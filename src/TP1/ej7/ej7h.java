6ebdb80eefe6862cc9ac13df6e360dd4d4b8ff17
Maxi <maxinunez2455@gmail.com>
2024-09-24 20:40:25 -0300

    Terminado el 7h

--- src/TP1/ej7/ej7h.java
+++ src/TP1/ej7/ej7h.java
@@ -4,21 +4,24 @@ import java.util.ArrayList;
 
 public class ej7h {
     public static void invertirArrayList(ArrayList<Integer> lista){
-        if(!lista.isEmpty()){
-            int i = lista.getLast();
-            lista.set(lista.size()-1, lista.getFirst());
-            lista.set(0, i);
-            lista.removeFirst();
-            invertirArrayList(lista);
+        invertirHelper(lista, 0, lista.size()-1);
+    }
+
+    private static void invertirHelper(ArrayList<Integer> lista, int start, int end){
+        if(start < end){
+            int aux = lista.get(start);
+            lista.set(start, lista.get(end));
+            lista.set(end, aux);
+            invertirHelper(lista, ++start, --end);
         }
     }
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<>();
-        lista.add(5);
-        lista.add(4);
-        lista.add(3);
-        lista.add(2);
         lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
         invertirArrayList(lista);
         System.out.println(lista);
     }
