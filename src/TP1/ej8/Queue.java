136140098a1e3088ca8761d1bff24fe9bf77a00c
Maxi <maxinunez2455@gmail.com>
2024-09-24 22:43:32 -0300

    Implementación de Queue

---
+++ src/TP1/ej8/Queue.java
@@ -0,0 +1,38 @@
+package TP1.ej8;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Queue<T> {
+    private List<T> data;
+
+    public Queue(){
+        data = new LinkedList<T>();
+    }
+
+    public int size(){
+        return data.size();
+    }
+
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+
+    public void enqueue(T dato){
+        data.add(dato);
+    }
+
+    public T dequeue(){
+        return data.removeFirst();
+    }
+
+    public T head(){
+        return data.getFirst();
+    }
+
+    public String toString(){
+        return data.toString();
+    }
+
+
+}
